0389688cc38d26d1815db9e8a1cf7d16a2783502
leonardo <leo.pohlmann@hotmail.com>
2024-04-24 18:58:05 -0300

    mensagens

--- main.c
+++ main.c
@@ -2,14 +2,17 @@
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <strings.h>
 #include <termios.h>
 #include <unistd.h>
 
 #define JOGADOR_1 'X'
 #define JOGADOR_2 'O'
+#define TAMANHO_MENSAGEM 21
 
-struct termios orig_termios;
-
+//
+// typedefs
+//
 typedef struct {
   int y;
   int x;
@@ -26,9 +29,12 @@ typedef struct {
   char vez_do_jogador;
   Coord posicao_atual;
   char cerquilha[3][3];
-  char mensagens[3][21];
+  char mensagens[3][TAMANHO_MENSAGEM];
 } ContextoJogo;
 
+//
+// Variáveis Globais
+//
 Movimentos MOVIMENTOS = {.cima = {.y = -1, .x = 0},
                          .direita = {.y = 0, .x = 1},
                          .baixo = {.y = 1, .x = 0},
@@ -36,6 +42,9 @@ Movimentos MOVIMENTOS = {.cima = {.y = -1, .x = 0},
 
 struct termios orig_termios;
 
+//
+// Funções
+//
 void POS_Mover(Coord *posicao, Coord *direcao) {
   int x_apos_movimento = posicao->x + direcao->x;
   int y_apos_movimento = posicao->y + direcao->y;
@@ -60,7 +69,6 @@ bool CQL_Marcar(char cerquilha[3][3], Coord *posicao_escolhida, char marcador) {
 }
 
 void CQL_Print(char cerquilha[3][3], Coord *posicao_escolhida) {
-  write(STDIN_FILENO, "\x1b[2J", 4);
   for (int i = 0; i < 3; i++) {
     for (int j = 0; j < 3; j++) {
       printf("---");
@@ -132,6 +140,69 @@ void CTX_Init(ContextoJogo *ctx) {
   ctx->posicao_atual.x = 1;
 
   ctx->vez_do_jogador = JOGADOR_1;
+
+  for (int i = 0; i < 3; i++) {
+    for (int j = 0; j < TAMANHO_MENSAGEM; j++) {
+      ctx->mensagens[i][j] = ' ';
+    }
+    ctx->mensagens[i][TAMANHO_MENSAGEM - 1] = '\0';
+  }
+}
+
+void MSG_Colocar(ContextoJogo *ctx, char *mensagem) {
+  for (int i = 0; i < TAMANHO_MENSAGEM; i++) {
+    ctx->mensagens[2][i] = ctx->mensagens[1][i];
+    ctx->mensagens[1][i] = ctx->mensagens[0][i];
+  }
+
+  int k = 0;
+  while (mensagem[k] != '\0') {
+    ctx->mensagens[0][k] = mensagem[k];
+    k++;
+  }
+  for (int i = k; i < TAMANHO_MENSAGEM; i++) {
+    ctx->mensagens[0][i] = ' ';
+  }
+  ctx->mensagens[0][TAMANHO_MENSAGEM - 1] = '\0';
+}
+
+void MSG_UltimaJogada(ContextoJogo *ctx) {
+  char msg_marcar[TAMANHO_MENSAGEM];
+  char t[] = "%c - %d %d";
+  snprintf(msg_marcar, TAMANHO_MENSAGEM, t, ctx->vez_do_jogador,
+           ctx->posicao_atual.y, ctx->posicao_atual.x);
+
+  MSG_Colocar(ctx, msg_marcar);
+}
+
+void MSG_Vitoria(ContextoJogo *ctx) {
+  char msg_marcar[TAMANHO_MENSAGEM];
+  char t[] = "%c Venceu!";
+  snprintf(msg_marcar, TAMANHO_MENSAGEM, t, ctx->vez_do_jogador);
+
+  MSG_Colocar(ctx, msg_marcar);
+}
+
+void MSG_Print(ContextoJogo *ctx) {
+  for (int i = 0; i < TAMANHO_MENSAGEM; i++) {
+    printf("-");
+  }
+  printf("\n");
+  for (int i = 0; i < 3; i++) {
+    printf("%s\n", ctx->mensagens[i]);
+  }
+  for (int i = 0; i < TAMANHO_MENSAGEM; i++) {
+    printf("-");
+  }
+  printf("\n");
+}
+
+void PrintJogo(ContextoJogo *ctx) {
+  write(STDIN_FILENO, "\x1b[2J", 4);
+  printf("\n");
+  printf("Vez do %c\n", ctx->vez_do_jogador);
+  CQL_Print(ctx->cerquilha, &ctx->posicao_atual);
+  MSG_Print(ctx);
 }
 
 void TerminalReset() { tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios); }
@@ -146,11 +217,15 @@ void TerminalSetup() {
   tcsetattr(STDIN_FILENO, TCSAFLUSH, &term_alterado);
 }
 
+//
+// Main
+//
 int main() {
   TerminalSetup();
 
   ContextoJogo ctx;
   CTX_Init(&ctx);
+  write(STDIN_FILENO, "\x1b[2J", 4);
   CQL_Print(ctx.cerquilha, &ctx.posicao_atual);
 
   while (1) {
@@ -165,10 +240,10 @@ int main() {
       return 0;
     } else if (c == '\n') {
       if (CQL_Marcar(ctx.cerquilha, &ctx.posicao_atual, ctx.vez_do_jogador)) {
-        CQL_Print(ctx.cerquilha, &ctx.posicao_atual);
+        MSG_UltimaJogada(&ctx);
         if (CQL_VitoriaFoiObtida(ctx.cerquilha)) {
-          printf("%c Venceu!\n", ctx.vez_do_jogador);
-          return 0;
+          MSG_Vitoria(&ctx);
+          break;
         }
 
         if (ctx.vez_do_jogador == JOGADOR_1) {
@@ -187,24 +262,26 @@ int main() {
         switch (seq[1]) {
         case 'A':
           POS_Mover(&ctx.posicao_atual, &MOVIMENTOS.cima);
-          CQL_Print(ctx.cerquilha, &ctx.posicao_atual);
           break;
         case 'B':
           POS_Mover(&ctx.posicao_atual, &MOVIMENTOS.baixo);
-          CQL_Print(ctx.cerquilha, &ctx.posicao_atual);
           break;
         case 'C':
           POS_Mover(&ctx.posicao_atual, &MOVIMENTOS.direita);
-          CQL_Print(ctx.cerquilha, &ctx.posicao_atual);
           break;
         case 'D':
           POS_Mover(&ctx.posicao_atual, &MOVIMENTOS.esquerda);
-          CQL_Print(ctx.cerquilha, &ctx.posicao_atual);
           break;
         }
       }
     }
+
+    //
+    // Print
+    //
+    PrintJogo(&ctx);
   }
+  PrintJogo(&ctx);
 
   return 0;
 }
